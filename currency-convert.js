adc960e55858ba4c5047cd58d55eb91e19b964a0
LeRon Jackson <leronj23@gmail.com>
2019-02-01 16:45:16 -0500

    update

--- currency-convert.js
+++ currency-convert.js
@@ -4,14 +4,27 @@ const axios = require("axios");
 var inquirer = require('inquirer');
 
 const getExchangeRate = async (from, to) => {
-    const response = await axios.get(`http://data.fixer.io/api/latest?access_key=${keys.fixerio.api}`)
-    const euro = 1 / response.data.rates[from];
-    return euro * response.data.rates[to];
+    try {
+        const response = await axios.get(`http://data.fixer.io/api/latest?access_key=${keys.fixerio.api}`)
+        const euro = 1 / response.data.rates[from];
+        const rate = euro * response.data.rates[to];
+
+        if (isNaN(rate)) {
+            throw new Error();
+        }
+        return rate;
+    } catch (e) {
+        throw new Error(`Unable to get exchange rate for ${from} and ${to}.`);
+    }
 }
 
 const getCountries = async (currencyCode) => {
-    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
-    return response.data.map((country) => country.name);
+    try {
+        const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
+        return response.data.map((country) => country.name);
+    } catch (e) {
+        throw new Error(`Unable to get countries that use ${currencyCode}.`);
+    }
 }
 
 const covertCurrency = async (from, to, amount) => {
